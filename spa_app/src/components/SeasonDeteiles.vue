58f7ab4ace05abd95bb52a06746344d4dbaf53c8
Thamer AL-Harbi <tamr4947@gmail.com>
2019-05-10 11:47:22 +0300

    Added: Update Delete in SeasonDetalies

--- spa_app/src/components/SeasonDeteiles.vue
+++ spa_app/src/components/SeasonDeteiles.vue
@@ -3,7 +3,15 @@
     <div class="title has-text-centered">
     {{season.name}}
     </div>
-    <div class="img">
+    <div class="">
+      <div class="button">
+        <i class="far fa-edit"></i>
+      </div>
+      <button v-on:click="deleteSeason()" class="button">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>
+    <div class="card img">
       <img :src="season.photo_url">
     </div>
     <div class="">
@@ -19,18 +27,23 @@
           <hr>
         </div>
         <div class="columns">
-          <router-link class="column is-one-quarter" v-for="episod in season.episodes"
-          :to="{ name: 'Video', params: {slug: episod.slug} }"
-          v-bind:key="episod.created">
+          <div class="column is-one-quarter" v-for="episode in season.episodes"
+          v-bind:key="episode.created">
           <div class="card">
             <div class="">
-              {{episod.name}}
+              {{episode.name}}
             </div>
-            <div class="">
-              <img :src="episod.photo_url" alt="">
-            </div>
+            <router-link :to="{ name: 'Video', params: {slug: episode.slug} }" class="img">
+              <img :src="episode.photo_url" alt="">
+            </router-link>
+            <button class="button">
+              <i class="far fa-edit"></i>
+            </button>
+            <button v-on:click="deleteVideo(episode.slug)" class="button">
+              <i class="fas fa-trash"></i>
+            </button>
           </div>
-        </router-link>
+        </div>
       </div>
 
       </div>
@@ -53,6 +66,44 @@ export default {
         .then(data => {
           console.log(data)
           this.season = data
+        })
+    },
+    updateSeason () {
+      api.putSeason(this.slug, this.season)
+        .then(data => {
+          this.season = data
+          console.log(data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    deleteSeason () {
+      api.deleteSeason(this.slug)
+        .then(data => {
+          console.log(data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    updateVideo (slug, video) {
+      api.putVideo(slug, video)
+        .then(data => {
+          video = data
+          console.log(data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    },
+    deleteVideo (slug) {
+      api.deleteVideo(slug)
+        .then(data => {
+          console.log(data)
+        })
+        .catch(error => {
+          console.log(error)
         })
     }
   },
